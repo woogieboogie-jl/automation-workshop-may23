eb0d4625f82e6b4a804edf332015ffccde58ce48
woogieboogie <woogieboogie.jl@gmail.com>
2025-05-21 23:10:07 +0900

    automation - timebased

---
+++ src/CountSimple.sol
@@ -0,0 +1,16 @@
+// // SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+contract CountSimple {
+  event CountedBy(address indexed msgSender);
+
+  uint256 public counted = 0;
+
+  constructor() {}
+
+  function performUpkeep() external {
+    counted += 1;
+    emit CountedBy(msg.sender);
+  }
+}
+
